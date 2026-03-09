fe530c8159031e611f3d901377156ec40e342603
thclassixs <thclassixs@gmail.com>
2026-03-09 23:53:58 +0000

    ddd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://zerowfx.com',
+    integrations: [sitemap()],
     server: {
         host: '0.0.0.0',
     },
